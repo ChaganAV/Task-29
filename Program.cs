4d039bfd4cc2a0b93f9fd8389c110e3dab16eeb5
Alexandr <jwchaganav@gmail.com>
2022-11-08 16:19:11 +0400

    Create

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите размер массива и мы заполним его чем попало:)");
-Console.WriteLine("А потом посмотрим, что из этого получится:)");
 int size = 0;
+int sizeRange = 100;
 if (Int32.TryParse(Console.ReadLine(), out size))
 {
-
+    int[] arrRandom = new int[size];
+    FillArray(arrRandom,sizeRange);
+    PrintArray(arrRandom);
 }
 else
 {
@@ -13,11 +15,26 @@ else
 }
 
 // Functions
-int[] FillArray(int[] arrayIn)
+// заполнение массива
+void FillArray(int[] arrayIn,int range)
 {
+    Random rnd = new Random();
     for (int i = 0; i < arrayIn.Length; i++)
     {
-        
+        arrayIn[i] = rnd.Next(0,range);
     }
-    return arrayIn;
+}
+
+// печать массива
+void PrintArray(int[] arrayIn)
+{
+    for(int i = 0; i < arrayIn.Length; i++)
+    {
+        if (i == 0)
+            Console.Write($"[{arrayIn[i]}, ");
+        else if (i == arrayIn.Length-1)
+            Console.Write($"{arrayIn[i]}]");
+        else
+            Console.Write($"{arrayIn[i]}, ");
+    }
 }

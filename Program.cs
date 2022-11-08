42920a04341c1265de7bba76db2854c768934ef1
Alexandr <jwchaganav@gmail.com>
2022-11-08 15:30:12 +0400

    Заготовки

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите размер массива и мы заполним его чем попало:)");
+Console.WriteLine("А потом посмотрим, что из этого получится:)");
+int size = 0;
+if (Int32.TryParse(Console.ReadLine(), out size))
+{
+
+}
+else
+{
+    Console.WriteLine("Необходимо ввести цифры!");
+    return;
+}
+
+// Functions
+int[] FillArray(int[] arrayIn)
+{
+    for (int i = 0; i < arrayIn.Length; i++)
+    {
+        
+    }
+    return arrayIn;
+}
